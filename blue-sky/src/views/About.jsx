826907e0ab28ec38c01975f5efbcf2f9b781c0c4
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-12-18 12:50:47 -0500

    made titles about page

--- blue-sky/src/views/About.jsx
+++ blue-sky/src/views/About.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import '../css/about.css';
 import about1 from '../img/about1.webp';
+import about2 from '../img/about2.webp';
+import about3 from '../img/about3.webp';
+import about4 from '../img/about4.webp';
+import about5 from '../img/about5.webp';
+import about6 from '../img/about6.webp';
+
 
 class About extends Component {
     constructor() {
@@ -56,7 +62,10 @@ class About extends Component {
             <div className="about-area" id="about">
                 <div className="aboutBlocks">
                     <div className="aboutBlock" style={{opacity: this.state.opacity1}}>
-                        <img src={about1} alt="about1" />
+                        <div className="imgContainer">
+                            <img src={about1} alt="about1" />
+                            <h1>OUR MISSION</h1>
+                        </div>
                         <p>
                             Blue Sky's mission is to provide high terpene, 
                             full spectrum, bud-run extraction.  We believe 
@@ -75,10 +84,16 @@ class About extends Component {
                             these properties to capture the essence of the full 
                             plant.
                         </p>
-                        <img src={about1} alt="about1" />
+                        <div className="imgContainer">
+                            <img src={about2} alt="about2" />
+                            <h1>WHAT WE DO</h1>
+                        </div>
                     </div>
                     <div className="aboutBlock" style={{opacity: this.state.opacity3}}>
-                        <img src={about1} alt="about1" />
+                        <div className="imgContainer">
+                            <img src={about3} alt="about3" />
+                            <h1>OUR VALUE</h1>
+                        </div>
                         <p>
                             Blue Sky's mission is to provide high terpene, 
                             full spectrum, bud-run extraction.  We believe 
@@ -97,10 +112,16 @@ class About extends Component {
                             these properties to capture the essence of the full 
                             plant.
                         </p>
-                        <img src={about1} alt="about1" />
+                        <div className="imgContainer">
+                            <img src={about4} alt="about4" />
+                            <h1>TEST RUNS</h1>
+                        </div>
                     </div>
                     <div className="aboutBlock" style={{opacity: this.state.opacity5}}>
-                        <img src={about1} alt="about1" />
+                        <div className="imgContainer">
+                            <img src={about5} alt="about5" />
+                            <h1>COMPLIANCE FOCUS</h1>
+                        </div>
                         <p>
                             Blue Sky's mission is to provide high terpene, 
                             full spectrum, bud-run extraction.  We believe 
@@ -119,7 +140,10 @@ class About extends Component {
                             these properties to capture the essence of the full 
                             plant.
                         </p>
-                        <img src={about1} alt="about1" />
+                        <div className="imgContainer">
+                            <img src={about6} alt="about6" />
+                            <h1>LOCAL MAINE PRESENCE</h1>
+                        </div>
                     </div>
                 </div>
             </div>
